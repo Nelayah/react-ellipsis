5dcabe8fdc85192e8007f4dd8312583898d22741
pengjiasheng <hayalen@163.com>
2018-10-28 18:51:15 +0800

    build(dist): npm run dist

--- jsx/ellipsis.jsx
+++ jsx/ellipsis.jsx
@@ -1,12 +1,3 @@
-var __rest = (this && this.__rest) || function (s, e) {
-    var t = {};
-    for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p) && e.indexOf(p) < 0)
-        t[p] = s[p];
-    if (s != null && typeof Object.getOwnPropertySymbols === "function")
-        for (var i = 0, p = Object.getOwnPropertySymbols(s); i < p.length; i++) if (e.indexOf(p[i]) < 0)
-            t[p[i]] = s[p[i]];
-    return t;
-};
 import * as React from 'react';
 export default class default_1 extends React.Component {
     constructor() {
@@ -44,10 +35,10 @@ export default class default_1 extends React.Component {
             this.ellipsisNode = node;
         };
         this.handleTextRender = () => {
-            const _a = this.props, { children } = _a, restProps = __rest(_a, ["children"]);
+            const { children, style } = this.props;
             if (typeof children !== 'string')
                 return null;
-            return (<div id="_react_ellipsis" ref={this.handleEllipsisNode} {...restProps}>
+            return (<div id="_react_ellipsis" ref={this.handleEllipsisNode} style={style}>
         {children}
       </div>);
         };
